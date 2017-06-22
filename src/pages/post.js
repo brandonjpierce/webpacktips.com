f29e14a2fc9b1e98e493d39b1e0d7878eb38b5e5
Brandon Pierce <brandonpierce91@gmail.com>
2017-06-21 20:53:27 -0500

    Misc updates to templating

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const toLower = require('lodash/toLower');
 const { send } = require('micro');
+const cache = require('../cache');
 const { readFile } = require('../utils');
 const notFound = require('./not-found');
 
@@ -10,11 +11,16 @@ module.exports = async (req, res) => {
   const filePath = path.resolve('dist', 'pages', lang, `${file}.html`);
   let data;
 
-  try {
-    data = await readFile(filePath, 'utf8');
-  } catch (err) {
-    req.log.error('Markdown file not found', { url: `/${lang}/${file}` });
-    return notFound(req, res);
+  if (cache.has(file)) {
+    data = cache.get(file);
+  } else {
+    try {
+      data = await readFile(filePath, 'utf8');
+      cache.set(file, data);
+    } catch (err) {
+      req.log.error('Markdown file not found', { url: `/${lang}/${file}` });
+      return notFound(req, res);
+    }
   }
 
   return send(res, 200, data);
